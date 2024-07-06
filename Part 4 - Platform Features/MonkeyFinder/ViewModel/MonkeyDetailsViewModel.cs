eb692ebc51dcfc28859180291d5d991d73e1fc37
Cardin Menkemeller <41063241+cmenkemeller@users.noreply.github.com>
2024-07-06 07:32:03 -0400

    Added connectivity, geolocation, and maps

--- Part 4 - Platform Features/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 4 - Platform Features/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,12 +1,30 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
 [QueryProperty(nameof(Monkey), "Monkey")]
-public partial class MonkeyDetailsViewModel : BaseViewModel
+public partial class MonkeyDetailsViewModel(IMap map) : BaseViewModel
 {
-    public MonkeyDetailsViewModel()
-    {
-    }
 
+    readonly IMap map = map;
     [ObservableProperty]
     Monkey monkey;
+
+    [RelayCommand]
+    async Task GetDirections()
+    {
+        if (Monkey == null)
+            return;
+        try
+        {
+            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude,
+            new MapLaunchOptions
+            {
+                Name = Monkey.Name,
+                NavigationMode = NavigationMode.None
+            });
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
+        }
+    }
 }
--- Part 4 - Platform Features/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 4 - Platform Features/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -5,23 +5,49 @@ namespace MonkeyFinder.ViewModel;
 public partial class MonkeysViewModel : BaseViewModel
 {
     public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    IConnectivity connectivity;
+    IGeolocation geolocation;
     MonkeyService monkeyService;
-    public MonkeysViewModel(MonkeyService monkeyService)
+    public MonkeysViewModel(MonkeyService monkeyService, IConnectivity connectivity, IGeolocation geolocation)
     {
         Title = "Monkey Finder";
         this.monkeyService = monkeyService;
+        this.connectivity = connectivity;
+        this.geolocation = geolocation;
     }
-    
+
     [RelayCommand]
     async Task GoToDetails(Monkey monkey)
     {
         if (monkey == null)
-        return;
+            return;
 
         await Shell.Current.GoToAsync(nameof(DetailsPage), true, new Dictionary<string, object>
         {
             {"Monkey", monkey }
         });
+    }
+
+    [RelayCommand]
+    async Task GetClosestMonkey()
+    {
+
+        var location = await geolocation.GetLastKnownLocationAsync();
+        var closestMonkey = Monkeys
+            .OrderBy(m => location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles))
+            .FirstOrDefault();
+
+        if (closestMonkey is not null)
+        {
+            var distance = Math.Round(location.CalculateDistance(closestMonkey.Latitude, closestMonkey.Longitude, DistanceUnits.Miles));
+            await Shell.Current.DisplayAlert("Closest Monkey", $"Closest Monkey: {closestMonkey.Name}\nDistance: {distance} miles", "OK");
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("No Monkeys", "There are no monkeys available.", "OK");
+        }
+
     }
 
     [RelayCommand]
@@ -33,12 +59,17 @@ public partial class MonkeysViewModel : BaseViewModel
         try
         {
             IsBusy = true;
+            if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("No Internet!", "Please check your internet connection", "OK");
+                return;
+            }
             var monkeys = await monkeyService.GetMonkeys();
 
-            if(Monkeys.Count != 0)
+            if (Monkeys.Count != 0)
                 Monkeys.Clear();
-                
-            foreach(var monkey in monkeys)
+
+            foreach (var monkey in monkeys)
                 Monkeys.Add(monkey);
 
         }
